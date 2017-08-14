555db0ead92ff411420698e30e1b8782c76efb5d
Tai <taitrananhvn@gmail.com>
2017-08-13 23:14:05 -0700

    add file

---
+++ crack_code/ch2.linked_list/2.1.cpp
@@ -0,0 +1,68 @@
+/* Write code to remove duplicates from an unsorted linked list. FOLLOW UP How would you solve this problem if a temporary buffer is not allowed?
+
+xóa ký tự trùng nhau trong 1 danh sách liên kết chưa sắp xếp.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+class linkedlist {
+	struct Node {
+		int data;
+		Node * link;
+	};
+private:
+	Node* head;
+
+public:
+	linkedlist() {
+		head = NULL;
+	}
+
+	void createNode(int data) {
+		Node* temp = new Node();
+		temp->data = data;
+		temp->link = NULL;
+
+		if (head == NULL)
+		{
+			head = temp;
+		} else {
+			Node* tmp = head;
+			while (tmp->link != NULL) {
+				tmp = tmp->link;
+			}
+			tmp->link = temp;
+
+		}
+
+	}
+
+	void printList() {
+		Node* tmp = head;
+		while (tmp != NULL) {
+			printf("%d\n",tmp->data);
+			tmp = tmp->link;
+		}
+	}
+
+};
+
+
+int main() {
+	linkedlist ll;
+	int a[]={2,3,4,5};
+
+
+	for (int i=0; i< (sizeof(a)/sizeof(a[0])); i++) { 
+		//printf("%d ",a[i]);
+		ll.createNode(a[i]);
+	}
+	
+	ll.printList();
+
+	
+
+	return 0;
+}

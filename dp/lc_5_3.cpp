a27f3e909d1fb34679f09cc7f6f868919c3f5931
Tai <tai2.tran@lge.com>
2019-10-24 17:34:25 +0700

    letcode: problem 5, longest palindromic Substring

--- dp/lc_5_3.cpp
+++ dp/lc_5_3.cpp
@@ -1,42 +1,48 @@
 /*
  * https://leetcode.com/problems/longest-palindromic-substring/
  * https://github.com/grandyang/leetcode/issues/5
- * Longest Palindromic Subsequence II: find the length of the longest palindromic subsequence
- * bottom-up DP
- * L[i,j]: maximum number of characters of a palindromic substring inside [i,j]
- * i.e abbccbx => L[0,6] = 4 (bccb)
- * if (s[i] == s[j]) L[i,j] = L[i+1,j-1] +2 // look not right when insde substring [i+1,j-1] doesn't contain a palindromic
- * else L[i,j] = max(L[i+1,j], L[i,j-1])
- * TODO: solution not correct yet, fix me!
+ * 
+ * expand palinddromic string from center
+ *
+ * abccbm: {c,c} => {bc,cb}:  expand(i,i+1)
+ * abcbm: {c} => {bcb}: expand(i,i)
+ *
  */
 
 
 #include <cstdio>
 #include <vector>
 #include <string>
-#include<algorithm>
 
 using namespace std;
 
 class Solution {
     public:
-        int longestPalindrome(string s) {
+        string longestPalindrome(string s) {
             int n = s.size(), maxlen=0, start=0;
             vector<vector<int>> dp(n,vector<int>(n,0));
-            for (int i=0; i<n;i++) dp[i][i] = 1;
             
-            for (int j=1; j<= n-1; j++) {
-                for (int i=j-1; i>=0; i--) {
-                    if (s[i]==s[j]) {dp[i][j] = dp[i+1][j-1] +2; printf("dp[%d][%d]=%d\n",i,j,dp[i][j]);}
-                    else {dp[i][j] = max(dp[i+1][j], dp[i][j-1]); printf("dp[%d][%d]=%d\n",i,j,dp[i][j]);}
-                }
+            for (int i=0; i<= n-1; i++) {
+                expand(i,i,start,maxlen,s);
+                expand(i,i+1,start,maxlen,s);
             }
-            return dp[0][n-1];
+            return s.substr(start,maxlen);
+        }
+
+        void expand(int left, int right, int& start, int& maxlen,string s) {
+            while (s[left]== s[right] && left>=0 && right<= s.size()) {
+                left--;
+                right++;
+            }
+            if ((right-left-2) > maxlen && (left+1 < right -1)) {
+                maxlen = right-1-left;
+                start = left+1;
+            }
         }
 };
 int main() {
     Solution anw;
-    printf("longest palindronme:%d\n",anw.longestPalindrome("abcbuabnnba"));
+    printf("longest palindronme:%s\n",anw.longestPalindrome("abcbuabnnba").c_str());
 
     return 0;
 }

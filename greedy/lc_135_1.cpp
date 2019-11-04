28b55bbad550a3d46f23c1992f2f8e1f188024e4
Tai <taitrananhvn@gmail.com>
2019-11-04 22:21:43 +0700

    lc 135

--- greedy/lc_135_1.cpp
+++ greedy/lc_135_1.cpp
@@ -10,17 +10,24 @@
 
 #include <vector>
 #include <stack>
+#include <cstdio>
 using namespace std;
 
 class Solution {
-    int candy(vector<int>& ratings) {
-        n = ratings.size();
+    public:
+    int getCandy(vector<int>& ratings) {
+        int n = ratings.size();
         vector<int> candy(n,1);
+        int num=0;
         for (int i=0; i<n;i++) 
             if (ratings[i+1] > ratings[i]) ++candy[i+1];
 
         for (int j=n-1; j>0;j--) 
-            if (ratings[j] < ratings[j-1]) 
+            if (ratings[j] < ratings[j-1]) candy[j-1] = max(candy[j-1], candy[j-1]+1); 
+
+        for (int i=0;i<n;i++) num += candy[i];
+
+        return num;
 
         
     }
@@ -28,6 +35,10 @@ class Solution {
 
 int main() {
     Solution aws;
+    vector<int> ratings={1,2,2};
+    printf("%d\n",aws.getCandy(ratings));
+
+    return 0;
 
 }
 

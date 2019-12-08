cdf99dbf9b167565b8018fdc337471846b073f0a
unknown <taitrananhvn@gmail.com>
2019-12-08 23:33:29 +0700

    lc 721

--- union_find/lc_721_1.cpp
+++ union_find/lc_721_1.cpp
@@ -9,19 +9,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+
 class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        unordered_map<string,string> root;
+        unordered_map<string,set<string>> m;
+        unordered_map<string,string> owner;
+        vector<vector<string>> res;
 
+        for(auto account: accounts) {
+            FOR(i,1,account.size()) {
+                owner[account[i]] = account[0];
+                root[account[i]] = account[i];
+            }
+        }
+
+        for (auto account: accounts) {
+            string x=findRoot(account[1],root);
+            FOR(i,2,account.size()) {
+                root[findRoot(account[i],root)] = x;
+            }
+        }
+
+        for(auto account: accounts) {
+            FOR(i,1,account.size()) {
+                string t=findRoot(account[i],root);
+                m[t].insert(account[i]);
+            }
+        }
+
+        for (auto a: m) {
+            vector<string> st(a.second.begin(),a.second.end());
+            st.insert(st.begin(),owner[st[0]]);
+            res.push_back(st);
+        }
+
+        return res;
+
+    }
+    string findRoot(string x,unordered_map<string,string> & root) {
+        return (x==root[x])? x: (root[x]=findRoot(root[x],root));
     }
 
 };
 
 int main() {
     Solution anw;
-    vector<vector<string>> accounts;
+    int n=0;
+    string st,word;
+
     freopen("lc_721.inp","r",stdin);
     freopen("lc_721.out","w",stdout);
+
+    scanf("%d\n",&n);
+    vector<vector<string>> accounts(n);
+    FOR(i,0,n) {
+        getline(cin,st);
+        stringstream str(st);
+        while (str>>word)
+            accounts[i].push_back(word);
+    }
+    vector<vector<string>> out=anw.accountsMerge(accounts);
+
+    for (auto a: out) {
+        FOR(i,0,a.size())
+            cout<<a[i]<<" ";
+        cout<<"\n";
+    }
 
 
     return 0;

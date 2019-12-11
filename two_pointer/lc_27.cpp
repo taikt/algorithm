5403ec15b421e2e178943c0084227ee705d620d2
unknown <taitrananhvn@gmail.com>
2019-12-11 23:48:27 +0700

    lc 27

--- two_pointer/lc_27.cpp
+++ two_pointer/lc_27.cpp
@@ -1,7 +1,7 @@
 /*
     https://leetcode.com/problems/remove-duplicates-from-sorted-array/
-    input: Given nums = [0,0,1,1,1,2,2,3,3,4]
-    output: {0,1,2,3,4}, length=5
+    input: nums = [3,2,2,3], val = 3,
+    output: {2,2}, length=2
 */
 
 #include <bits/stdc++.h>
@@ -9,29 +9,28 @@ using namespace std;
 
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
+    int removeElement(vector<int>& nums,int val) {
         int j=0;
         if (nums.size() ==0) return 0;
-        //for (int i=0; i<nums.size(); i++)
-          //  cout<<nums[i]<<" ";
-        //cout<<"\n";
 
-        for (int i=1; i<nums.size(); i++) {
-            if (nums[j] != nums[i]) {
-                j++;
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i] != val) {
                 nums[j] = nums[i];
+                j++;
             }
         }
-        return (j+1);
+        return (j);
     }
 
 };
 
 
 int main() {
-    freopen("lc_26.inp","r",stdin);
-    freopen("lc_26.out","w",stdout);
+    freopen("lc_27.inp","r",stdin);
+    freopen("lc_27.out","w",stdout);
     vector<int> nums;
+    int n,value;
+    scanf("%d\n",&value);
     string st;
     getline(cin,st);
     stringstream str(st);
@@ -40,7 +39,7 @@ int main() {
         nums.push_back(t);
 
     Solution anw;
-    int len = anw.removeDuplicates(nums);
+    int len = anw.removeElement(nums,value);
     cout<<len<<"\n";
     for (int i=0; i<len; i++)
             cout<<nums[i]<<" ";

7f788022a28b292f2a87d84caa76502f090480b2
unknown <taitrananhvn@gmail.com>
2019-12-15 23:40:52 +0700

    lc 3

--- sliding_window/lc_3_1.cpp
+++ sliding_window/lc_3_1.cpp
@@ -10,18 +10,11 @@ public:
 
         while (j<n) {
             auto it=find(m.begin(),m.end(),s[j]); // if m is set, can use m.find(s[j])
-            //it tra lai vi tri tai noi xuat hin s[j]
             if ( it != m.end()) //m.count(s)
             {
-                //cout<<*it;
-                //for (auto ii=it;ii!= m.end();ii++)
-                  //  cout<<*ii;
-                //cout<<"\n";
-                cout<<*it<<" ";
                 it++;
                 m.erase(m.begin(),it);
-                //for(int i=0;i<m.size();i++) cout<<m[i];
-                //cout<<"\n";
+                //after erase: m[*(it+1),..end], next position from where s[j] occurs
 
             }
             m.push_back(s[j]);

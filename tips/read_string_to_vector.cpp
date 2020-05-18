48b7852bb66b5e54f5db36b7a3c005f66baf1340
unknown <taitrananhvn@gmail.com>
2020-05-19 06:53:10 +0700

    tips/ string lines

--- tips/read_string_to_vector.cpp
+++ tips/read_string_to_vector.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
+    // convert string with split
+    // i.e "a b c d" => v[0]=a, v[1]=b...
+    #if 1
     freopen("read_string_to_vector.inp","r",stdin);
     freopen("read_string_to_vector.out","w",stdout);
     vector<int> nums;
@@ -30,6 +33,25 @@ int main() {
     for (auto a: strs) {
         cout<<a<<" ";
     }
+    #endif // 1
+
+    // convert a string line to a vector
+    // i.e "abc" => v="abc"
+    #if 0
+    //freopen("lc_190.inp","r",stdin);
+    //freopen("lc_190.out","w",stdout);
+    string s; //="input";
+    getline(cin,s);
+    vector<char> v(s.begin(),s.end());
+
+    Solution su;
+    su.reverseString(v);
+    for (auto c:v)
+        cout<<c;
+
+    return 0;
+
+    #endif // 0
 
     return 0;
 }

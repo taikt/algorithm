1003ae728fc7536acd97a4a4f51f0253549c6876
tai2.tran <tai2.tran@lge.com>
2020-06-16 17:44:56 +0700

    lc 252

--- codejam/lc_252.cpp
+++ codejam/lc_252.cpp
@@ -1,5 +1,11 @@
 #include "bits/stdc++.h"
 using namespace std;
+/*
+    input la day cac time interval cua cac buoi hop khac nhau.
+    Kiem tra xem 1 nguoi co the attend tat ca cac buoi hop hay khong.
+    Solution: sap xep cac interval theo tang dan start time.
+    neu ton tai start time cua interval i nho hon end time cua interval i-1,=> khong thoa man
+*/
 
 bool canAttendMeetings(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b){return a[0] < b[0];});
@@ -12,6 +18,17 @@ bool canAttendMeetings(vector<vector<int>>& intervals) {
 }
 
 int main() {
+    vector<vector<int>> meetings;// = {{1,4},{5,10},{12,18}};
+    int a,b;
+
+    //freopen("lc_252.txt","r",stdin);
+    // de tao EOF trong window stdin, new line: ctr+Z, Enter
+    while (scanf("%d %d",&a,&b) != EOF) {
+        vector<int> v{a,b};
+        meetings.push_back(v);
+    }
+    if (canAttendMeetings(meetings)) cout<<"ok";
+    else cout<<"not ok";
 
     return 0;
 }

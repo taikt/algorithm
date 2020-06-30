b48d0f0f0b7659f7db8204b5458f37232d49fd56
unknown <taitrananhvn@gmail.com>
2020-06-30 07:11:35 +0700

    lc 261

--- codejam/leetcode/lc_261.cpp
+++ codejam/leetcode/lc_261.cpp
@@ -1,21 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define N=5
+#define N 5
 vector<vector<int>> v(N, vector<int>());
 queue<int> q;
-vector<int> visited;
+vector<int> visited(N,0);
+vector<int> pre(N,0);
 
 bool bfs(int u) {
-    q.push(u);
-    while (!.q.empty()) {
+    q.push(u);visited[u]=1;
+    int t=0;
+    while (!q.empty()) {
         t=q.front(); q.pop();
-        visited[t] = 1;
+        //visited[t] = 1;
         for (auto x: v[t]) {
-            pre[x] = t;
-            q.push(x);
+            if (x != pre[t] && visited[x]) {
+                return false;
+            }
+            else if (!visited[x]){
+                pre[x] = t;
+                q.push(x); visited[x] = 1;
+            }
         }
     }
+    return true;
 
 }
 
@@ -25,8 +33,8 @@ int main() {
     v[3].push_back(1); v[3].push_back(2); v[3].push_back(4);
     v[4].push_back(3);
 
-    if (bfs(1)) cout<<"unvalid tree\n";
-    else cout<<"valid tree\n";
+    if (bfs(1)) cout<<"valid tree\n";
+    else cout<<"unvalid tree\n";
 
     return 0;
 }

bd77765f2a3587e7dce383ebbbeb6b00f7cfc279
tai2.tran <tai2.tran@lge.com>
2021-06-07 21:08:53 +0700

    Create customized exception class

--- miscellaneous/cxx/exception/divide.cpp
+++ miscellaneous/cxx/exception/divide.cpp
@@ -11,7 +11,7 @@ int main() {
 
 	int x=50;
 	int y=0;
-#if 0
+#if 1
 	try {
 		// printf("value=%f\n",division(x,y));
 		if (y==0) throw "division to zero";
@@ -21,13 +21,15 @@ int main() {
 		printf("%s\n",msg);
 	}
 #endif
+
+#if 0
 	try {
 		throw(12);
 
 	} catch (int i) {
 		printf("%d\n",i);
-		throw;
+		//throw;
 	}
-
+#endif // 0
 	return 0;
 }
